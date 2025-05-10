16f837f9e45512750dd96ad6f7ac8dc979195c0d
Supravab <ranjitapokharel16@gmail.com>
2025-05-10 17:07:43 +0545

    macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world!"); //here ! is a macro
+    let a=10;
+    let b=20;
+    if a<b {
+        println!("{} is the smallest", a);
+    }
+    else {
+        println!("{} is the smallest",b);
+    }
 }
+//what is a macro?
+// macro is basically a function that can take in variable number of arguements, it can check syntax before running.
